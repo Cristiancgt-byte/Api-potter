b46062181571069184612c5bcd93a9cd72a3c902
Cristiancgt-byte <cristian.gambat@uniagustiniana.edu.co>
2026-04-13 20:58:20 +0000

    cambios

--- src/equipo/index.tsx
+++ src/equipo/index.tsx
@@ -1,15 +1,56 @@
-import "./style.css";
+import { useParams } from 'react-router';
+import { useEffect, useState } from "react";
+
+interface TeamData {
+  team: {
+    name: string;
+    info: {
+      city: string;
+      founded: string;
+      stadium: string;
+      president: string;
+      last_title: string;
+    };
+    ranking: {
+      position: string;
+      competition: string;
+    };
+    social: {
+      facebook: string;
+      instagram: string;
+      x: string;
+    };
+    links: {
+      store: string;
+      tickets: string;
+    };
+  };
+}
 
 function Equipo() {
+  const { equipo } = useParams<{ equipo: string }>();
 
-    return (
+  const [data, setData] = useState<TeamData | null>(null);
 
-        <>
+  useEffect(() => {
+  if (!equipo) return;
 
-           <p>Equipo</p>
-        
-        </>
-    )
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        `https://raw.githubusercontent.com/sdtibata/dataliga/main/${equipo}.json`
+      );
+
+      const data = await res.json();
+      setData(data);
+    } catch (error) {
+      console.error("Error cargando datos:", error);
+    }
+  };
+
+  fetchData();
+}, [equipo]);
 }
+
 
 export default Equipo
--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,15 +1,88 @@
-import "./style.css";
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router'
+
+interface Ranking {
+  rank: number
+  contestantName: string
+  points: number
+  matchesPlayed: number
+}
 
 function Home() {
+  const [ranking, setRanking] = useState<Ranking[]>([])
+  const [title, setTitle] = useState('')
+ 
 
-    return (
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://raw.githubusercontent.com/sdtibata/dataliga/refs/heads/main/{equipo}.json')
+        const data = await res.json()
 
-        <>
+        setRanking(data.standings[0].ranking)
+        setTitle(data.standings[0].competitionName)
+      } catch (error) {
+        console.error('Error cargando datos:', error)
+      }
+    }
 
-           <p>Home</p>
-        
-        </>
-    )
+    fetchData()
+  }, [])
+
+  return (
+    <div className="tabla-container">
+      <h2>{title}</h2>
+      <table className="tabla-posiciones">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Equipo</th>
+            <th>PJ</th>
+            <th>Pts</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ranking.map((equipo) => (
+            <tr key={equipo.rank}>
+              <td>{equipo.rank}</td>
+              <td>
+                  <Link to={`/equipo/${equiposMap[equipo.contestantName] || "default"}`}>
+                      {equipo.contestantName}
+                  </Link>
+                  
+              </td>
+              <td>{equipo.matchesPlayed}</td>
+              <td>{equipo.points}</td>
+            </tr>
+            
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default Home
+
+const equiposMap: Record<string, string> = {
+  "América de Cali SA": "america-de-cali",
+  "CA Bucaramanga": "atletico-bucaramanga",
+  "Club Atlético Nacional SA": "atletico-nacional",
+  "Club Deportes Tolima SA": "deportes-tolima",
+  "Asociación Deportivo Cali": "deportivo-cali",
+  "Deportivo Independiente Medellín": "independiente-medellin",
+  "Club Independiente Santa Fe": "independiente-santa-fe",
+  "CD Popular Junior FC SA": "junior",
+  "Millonarios FC": "millonarios",
+  "Once Caldas SA": "once-caldas",
+  "AD Pasto": "Pasto",
+  "Internacional de Bogotá": "internacional-bogota",
+  "Club Llaneros SA": "llaneros",
+  "Águilas Doradas": "aguilas-doradas",
+  "Fortaleza FC": "fortaleza",
+  "Alianza FC": "alianza",
+  "Jaguares de Córdoba FC": "jaguares",
+  "Cúcuta Deportivo FC": "cucuta",
+  "Boyacá Chicó FC": "boyaca-chico",
+  "Deportivo Pereira FC": "pereira"
+};
