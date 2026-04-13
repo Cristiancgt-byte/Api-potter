49876b5dc5d544697ec6951a5490e55d56c886d1
Cristiancgt-byte <cristian.gambat@uniagustiniana.edu.co>
2026-04-13 21:06:51 +0000

    cambios 2

--- src/equipo/index.tsx
+++ src/equipo/index.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router';
+import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 
 interface TeamData {
@@ -50,7 +50,58 @@ function Equipo() {
 
   fetchData();
 }, [equipo]);
+
+  if (!data) return <p>Cargando...</p>;
+
+  return (
+    <div>
+      <h1>{data.team.name}</h1>
+
+      <h2>Información</h2>
+      <p><strong>Ciudad:</strong> {data.team.info.city}</p>
+      <p><strong>Fundado:</strong> {data.team.info.founded}</p>
+      <p><strong>Estadio:</strong> {data.team.info.stadium}</p>
+      <p><strong>Presidente:</strong> {data.team.info.president}</p>
+      <p><strong>Último título:</strong> {data.team.info.last_title}</p>
+
+      <h2>Ranking</h2>
+      <p><strong>Posición:</strong> {data.team.ranking.position}</p>
+      <p><strong>Competencia:</strong> {data.team.ranking.competition}</p>
+
+      <h2>Redes</h2>
+      <ul>
+        <li>
+          <a href={data.team.social.facebook} target="_blank" rel="noreferrer">
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a href={data.team.social.instagram} target="_blank" rel="noreferrer">
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a href={data.team.social.x} target="_blank" rel="noreferrer">
+            X (Twitter)
+          </a>
+        </li>
+      </ul>
+
+      <h2>Extras</h2>
+      <ul>
+        <li>
+          <a href={data.team.links.store} target="_blank" rel="noreferrer">
+            Tienda oficial
+          </a>
+        </li>
+        <li>
+          <a href={data.team.links.tickets} target="_blank" rel="noreferrer">
+            Comprar boletas
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-
-export default Equipo
+export default Equipo;
--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router'
 
+import "./style.css";
+
 interface Ranking {
   rank: number
   contestantName: string
@@ -8,61 +10,16 @@ interface Ranking {
   matchesPlayed: number
 }
 
-function Home() {
-  const [ranking, setRanking] = useState<Ranking[]>([])
-  const [title, setTitle] = useState('')
- 
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch('https://raw.githubusercontent.com/sdtibata/dataliga/refs/heads/main/{equipo}.json')
-        const data = await res.json()
-
-        setRanking(data.standings[0].ranking)
-        setTitle(data.standings[0].competitionName)
-      } catch (error) {
-        console.error('Error cargando datos:', error)
-      }
-    }
-
-    fetchData()
-  }, [])
-
-  return (
-    <div className="tabla-container">
-      <h2>{title}</h2>
-      <table className="tabla-posiciones">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Equipo</th>
-            <th>PJ</th>
-            <th>Pts</th>
-          </tr>
-        </thead>
-        <tbody>
-          {ranking.map((equipo) => (
-            <tr key={equipo.rank}>
-              <td>{equipo.rank}</td>
-              <td>
-                  <Link to={`/equipo/${equiposMap[equipo.contestantName] || "default"}`}>
-                      {equipo.contestantName}
-                  </Link>
-                  
-              </td>
-              <td>{equipo.matchesPlayed}</td>
-              <td>{equipo.points}</td>
-            </tr>
-            
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+interface Estadistica {
+  position: number
+  name: string
+  value: number
+  appearances: number
+  contestantName: string
+  statName: string
 }
 
-export default Home
+type FiltroTipo = 'posiciones' | 'goleador' | 'asistencias' | 'amarillas' | 'atajadas'
 
 const equiposMap: Record<string, string> = {
   "América de Cali SA": "america-de-cali",
@@ -75,7 +32,7 @@ const equiposMap: Record<string, string> = {
   "CD Popular Junior FC SA": "junior",
   "Millonarios FC": "millonarios",
   "Once Caldas SA": "once-caldas",
-  "AD Pasto": "Pasto",
+
   "Internacional de Bogotá": "internacional-bogota",
   "Club Llaneros SA": "llaneros",
   "Águilas Doradas": "aguilas-doradas",
@@ -86,3 +43,145 @@ const equiposMap: Record<string, string> = {
   "Boyacá Chicó FC": "boyaca-chico",
   "Deportivo Pereira FC": "pereira"
 };
+
+function Home() {
+  const [ranking, setRanking] = useState<Ranking[]>([])
+  const [title, setTitle] = useState('')
+
+  //filtro
+  const [filtro, setFiltro] = useState<FiltroTipo>('posiciones')
+  const [estadisticas, setEstadisticas] = useState<Estadistica[]>([])
+
+  const filtros: FiltroTipo[] = ['posiciones', 'goleador', 'asistencias', 'amarillas', 'atajadas']
+
+  const [busqueda, setBusqueda] = useState('')
+
+  useEffect(() => {
+    setBusqueda('') 
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`https://raw.githubusercontent.com/sdtibata/dataliga/refs/heads/main/${filtro}.json`)
+        const data = await res.json()
+
+        if (filtro === 'posiciones') {
+          setRanking(data.standings[0].ranking)
+          setTitle(data.standings[0].competitionName)
+        } else {
+          setEstadisticas(data)
+        }
+      } catch (error) {
+        console.error('Error cargando datos:', error)
+      }
+    }
+
+    fetchData()
+  }, [filtro])
+
+  const rankingFiltrado = ranking.filter((equipo) =>
+    busqueda.length < 3
+      ? true  // muestra todos si hay menos de 3 caracteres
+      : equipo.contestantName.toLowerCase().includes(busqueda.toLowerCase())
+  )
+
+  const estadisticasFiltradas = estadisticas.filter((jugador) =>
+    busqueda.length < 3
+      ? true  // muestra todos
+      : jugador.name.toLowerCase().includes(busqueda.toLowerCase()) ||
+        jugador.contestantName.toLowerCase().includes(busqueda.toLowerCase())
+  )
+
+
+
+  return (
+    <>
+      <div className="filtros">
+        {filtros.map((onestat) => (
+          <button
+            key={onestat}
+            onClick={() => setFiltro(onestat)}
+            className={filtro === onestat ? 'activo' : ''}
+          >
+            {onestat}
+          </button>
+        ))}
+      </div>
+
+      <input
+        type="text"
+        placeholder="Buscar..."
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+      />
+
+      <div className="tabla-container">
+        <h2>{title}</h2>
+        {filtro === 'posiciones' ? (
+          <table className="tabla-posiciones">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Equipo</th>
+                <th>PJ</th>
+                <th>Pts</th>
+              </tr>
+            </thead>
+            <tbody>
+              {ranking.map((equipo) => (
+                <tr key={equipo.rank}
+                    className={
+                      busqueda.length >= 3 &&
+                      equipo.contestantName.toLowerCase().includes(busqueda.toLowerCase())
+                        ? 'resaltado'
+                        : ''
+                    }
+                >
+                  <td>{equipo.rank}</td>
+                  <td>
+                        <Link to={`/equipo/${equiposMap[equipo.contestantName] || "default"}`}>
+                        {equipo.contestantName}
+                      </Link>
+                  </td>
+                  <td>{equipo.matchesPlayed}</td>
+                  <td>{equipo.points}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <table className="tabla-estadisticas">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Jugador</th>
+                <th>Equipo</th>
+                <th>PJ</th>
+                <th>{filtro}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {estadisticas.map((jugador, index) => (
+                <tr key={index}
+                    className={
+                      busqueda.length >= 3 &&
+                      (jugador.name.toLowerCase().includes(busqueda.toLowerCase()) ||
+                      jugador.contestantName.toLowerCase().includes(busqueda.toLowerCase()))
+                        ? 'resaltado'
+                        : ''
+                    }
+                >
+                  <td>{jugador.position}</td>
+                  <td>{jugador.name}</td>
+                  <td>{jugador.contestantName}</td>
+                  <td>{jugador.appearances}</td>
+                  <td>{jugador.value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Home
